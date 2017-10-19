1c0ee2ee9198268ef6cded2c3cf8af7b3c66ca6f
minhhuynh25 <minh@inspilab.com>
2017-10-19 14:51:53 +0700

    add URL

--- posts/urls.py
+++ posts/urls.py
@@ -7,7 +7,7 @@ posts_list,posts_create,posts_detail,posts_update,posts_delete
 urlpatterns = [
     url(r'^$', posts_list),
     url(r'^create/$', posts_create),
-    url(r'^detail/$',posts_detail),
+    url(r'^(?P<id>\d+)/$',posts_detail, name="detail"),
     url(r'^update/$',posts_update),
     url(r'^delete/$',posts_delete),
 
