7d3bff2b13f6175fc42e85eb5ad1b0a9824f106d
minhhuynh25 <minh@inspilab.com>
2017-10-22 14:19:47 +0700

    Complete Base blog

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 
 
 def upload_location(instance, filename):
-    return "%s/%s.%s" %(instance.id, filename)
+    return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
